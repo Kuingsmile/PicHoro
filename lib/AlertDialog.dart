14135b56bf46787c93bfd2ae57262da42856339d
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-03 14:00:46 +0800

    通知弹出框界面增加了文字滚动，调整了字体

--- lib/AlertDialog.dart
+++ lib/AlertDialog.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
 
-showAlertDialog({required BuildContext context, required String title, required String content}) {
+showAlertDialog(
+    {required BuildContext context,
+    required String title,
+    required String content}) {
   return showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(title),
-          content: Text(content),
+          contentPadding: const EdgeInsets.only(left: 20, right: 20),
+          title: Center(
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 23.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          content: Container(
+            height: 200,
+            width: 300,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(content)
+              ],
+            ),
+          ),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('确定'),
+              child: const Center(
+                child: Text(
+                  '确  定',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ],
