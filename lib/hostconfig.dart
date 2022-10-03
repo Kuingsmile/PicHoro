f4ab6d3392b41285e1d3ea1108ec99aa1f82971f
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-03 14:01:49 +0800

    增加了对各种错误的异常处理和弹出框提示，增加了获取储存策略id列表的功能，调整了布局

--- lib/hostconfig.dart
+++ lib/hostconfig.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:dio/dio.dart';
 import 'dart:convert';
+import 'package:horopic/AlertDialog.dart';
 
 //import 'package:permission_handler/permission_handler.dart';
 //a textfield to get hosts,username,passwd,token and strategy_id
@@ -37,13 +38,14 @@ class _HostConfigState extends State<HostConfig> {
       ),
       body: Form(
         key: _formKey,
-        child: Column(
+        child: ListView(
           children: [
             TextFormField(
               controller: _hostController,
               decoration: const InputDecoration(
                 hintText: '域名(eg:https://imgx.horosama.com )',
               ),
+              textAlign: TextAlign.center,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return '请输入域名';
@@ -56,6 +58,7 @@ class _HostConfigState extends State<HostConfig> {
               decoration: const InputDecoration(
                 hintText: '用户名',
               ),
+              textAlign: TextAlign.center,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return '请输入用户名';
@@ -69,6 +72,7 @@ class _HostConfigState extends State<HostConfig> {
               decoration: const InputDecoration(
                 hintText: '密码',
               ),
+              textAlign: TextAlign.center,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return '请输入密码';
@@ -79,8 +83,10 @@ class _HostConfigState extends State<HostConfig> {
             TextFormField(
               controller: _strategyIdController,
               decoration: const InputDecoration(
-                hintText: 'Strategy Id',
+                contentPadding: EdgeInsets.zero,
+                hintText: '储存策略Id,可先输入前三项获取完整列表',
               ),
+              textAlign: TextAlign.center,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return '请输入储存策略Id';
@@ -94,12 +100,78 @@ class _HostConfigState extends State<HostConfig> {
                   _saveHostConfig();
                 }
               },
-              child: const Text('Submit'),
+              child: const Text('提交完整表单'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _getStrategyId();
+              },
+              child: const Text('获取储存策略Id列表'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _getStrategyId() async {
+    final host = _hostController.text;
+    String token = 'Bearer ';
+    final username = _usernameController.text;
+    final passwd = _passwdController.text;
+    BaseOptions options = BaseOptions();
+    options.headers = {
+      "Accept": "application/json",
+    };
+    Dio dio = Dio(options);
+    String fullUrl = '$host/api/v1/tokens';
+    FormData formData = FormData.fromMap({
+      'email': username,
+      'password': passwd,
+    });
+    try {
+      var response = await dio.post(
+        fullUrl,
+        data: formData,
+      );
+      if (response.statusCode == 200 && response.data['status'] == true) {
+        token = token + response.data['data']['token'].toString();
+        String strategiesUrl = '$host/api/v1/strategies';
+        BaseOptions strategiesOptions = BaseOptions();
+        strategiesOptions.headers = {
+          "Accept": "application/json",
+          "Authorization": token,
+        };
+        dio = Dio(strategiesOptions);
+        response = await dio.get(strategiesUrl);
+
+        showAlertDialog(
+            context: context,
+            title: '储存策略Id列表',
+            content: response.data['data']['strategies'].toString());
+      } else {
+        if (response.statusCode == 403) {
+          showAlertDialog(context: context, title: '错误', content: '管理员关闭了接口功能');
+          return;
+        } else if (response.statusCode == 401) {
+          showAlertDialog(context: context, title: '错误', content: '授权失败');
+          return;
+        } else if (response.statusCode == 500) {
+          showAlertDialog(context: context, title: '错误', content: '服务器异常');
+          return;
+        } else if (response.statusCode == 404) {
+          showAlertDialog(context: context, title: '错误', content: '接口不存在');
+          return;
+        }
+        if (response.data['status'] == false) {
+          showAlertDialog(
+              context: context, title: '错误', content: response.data['message']);
+          return;
+        }
+      }
+    } catch (e) {
+      showAlertDialog(context: context, title: '错误', content: e.toString());
+    }
   }
 
   void _saveHostConfig() async {
@@ -118,17 +190,42 @@ class _HostConfigState extends State<HostConfig> {
       'email': username,
       'password': passwd,
     });
-    var response = await dio.post(
-      fullUrl,
-      data: formData,
-    );
-    token = token + response.data['data']['token'].toString();
-    final hostConfig = HostConfigModel(host, token, strategyId);
-    final hostConfigJson = jsonEncode(hostConfig);
+    try {
+      var response = await dio.post(
+        fullUrl,
+        data: formData,
+      );
+      if (response.statusCode == 200 && response.data['status'] == true) {
+        token = token + response.data['data']['token'].toString();
+        final hostConfig = HostConfigModel(host, token, strategyId);
+        final hostConfigJson = jsonEncode(hostConfig);
 
-    final hostConfigFile = await _localFile;
-    hostConfigFile.writeAsString(hostConfigJson);
-    Navigator.pop(context);
+        final hostConfigFile = await _localFile;
+        hostConfigFile.writeAsString(hostConfigJson);
+        Navigator.pop(context);
+      } else {
+        if (response.statusCode == 403) {
+          showAlertDialog(context: context, title: '错误', content: '管理员关闭了接口功能');
+          return;
+        } else if (response.statusCode == 401) {
+          showAlertDialog(context: context, title: '错误', content: '授权失败');
+          return;
+        } else if (response.statusCode == 500) {
+          showAlertDialog(context: context, title: '错误', content: '服务器异常');
+          return;
+        } else if (response.statusCode == 404) {
+          showAlertDialog(context: context, title: '错误', content: '接口不存在');
+          return;
+        }
+        if (response.data['status'] == false) {
+          showAlertDialog(
+              context: context, title: '错误', content: response.data['message']);
+          return;
+        }
+      }
+    } catch (e) {
+      showAlertDialog(context: context, title: '错误', content: e.toString());
+    }
   }
 
   Future<File> get _localFile async {
