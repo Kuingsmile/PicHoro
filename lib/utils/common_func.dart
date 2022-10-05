9deeff4cd5d2f83011a4d0323fd123f8fc19d396
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-05 20:48:23 +0800

    通用代码

--- lib/utils/common_func.dart
+++ lib/utils/common_func.dart
@@ -1,9 +1,25 @@
-import 'dart:io';
+import 'dart:io' as io;
+import 'package:dio/dio.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:horopic/AlertDialog.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:horopic/utils/common_func.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path/path.dart';
+//import 'package:horopic/hostconfig.dart';
+import 'package:path_provider/path_provider.dart';
+//import 'package:permission_handler/permission_handler.dart';
+import 'package:horopic/utils/permission.dart';
+import 'package:horopic/utils/common_func.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
+//import 'package:camera/camera.dart';
+import 'package:horopic/pages/configurePage.dart';
+import 'package:horopic/pages/themeSet.dart';
+import 'package:horopic/pages/loading.dart';
+import 'package:horopic/utils/global.dart';
 
-bool imageConstraint({required BuildContext context, required File image}) {
+//图片检查
+bool imageConstraint({required BuildContext context, required io.File image}) {
   if (!['bmp', 'jpg', 'jpeg', 'png', 'gif', 'webp']
       .contains(image.path.split('.').last.toString())) {
     showAlertDialog(
@@ -13,4 +29,92 @@ bool imageConstraint({required BuildContext context, required File image}) {
     return false;
   }
   return true;
+}
+
+//弹出对话框
+showAlertDialog({
+  bool? barrierDismissible,
+  required BuildContext context,
+  required String title,
+  required String content,
+}) {
+  return showDialog(
+      context: context,
+      barrierDismissible: barrierDismissible == true ? true : false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.only(left: 20, right: 20),
+          title: Center(
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 23.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          content: Container(
+            height: 200,
+            width: 300,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(content)
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Center(
+                child: Text(
+                  '确  定',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      });
+}
+
+//底部选择框
+void bottomPickerSheet(BuildContext context, Function _imageFromCamera,
+    Function _imageFromGallery) {
+  showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+            child: Wrap(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.photo_camera),
+              title: const Text('拍照'),
+              onTap: () {
+                _imageFromCamera();
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.photo_library),
+              title: const Text('相册'),
+              onTap: () {
+                _imageFromGallery();
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ));
+      });
 }
