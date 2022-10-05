27f6520405a787125d065bc18ad110d32734372a
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-05 20:49:03 +0800

    全局变量保存

---
+++ lib/utils/global.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'package:flustars_flutter3/flustars_flutter3.dart';
+//全局共享变量
+
+class Global {
+  static File? imageFile;
+  static List<File> imagesList = [];
+  static String defaultPShost = 'lsky.pro';
+  static String defaultUser = ' ';
+  static String defaultPassword = ' ';
+
+  static getPShost() async {
+    await SpUtil.getInstance();
+    String pshost = SpUtil.getString('key_pshost', defValue: 'lsky.pro')!;
+    return pshost;
+  }
+
+  static getUser() async {
+    await SpUtil.getInstance();
+    String user = SpUtil.getString('key_user', defValue: ' ')!;
+    return user;
+  }
+
+  static getPassword() async {
+    await SpUtil.getInstance();
+    String password = SpUtil.getString('key_password', defValue: ' ')!;
+    return password;
+  }
+
+  static setPShost(String pshost) async {
+    await SpUtil.getInstance();
+    SpUtil.putString('key_pshost', pshost);
+    defaultPShost = pshost;
+  }
+
+  static setUser(String user) async {
+    await SpUtil.getInstance();
+    SpUtil.putString('key_user', user);
+    defaultUser = user;
+  }
+
+  static setPassword(String password) async {
+    await SpUtil.getInstance();
+    SpUtil.putString('key_password', password);
+    defaultPassword = password;
+  }
+}
