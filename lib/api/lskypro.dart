2a2c47773de11e5916289489e6621af89e6d0792
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-05 20:40:04 +0800

    兰空图床的接口单独分离了出来

--- lib/api/lskypro.dart
+++ lib/api/lskypro.dart
@@ -1,5 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class LskyproImageUploadUtils {
-
+  //上传接口
+  uploadApi(
+      {required String path,
+      required String name,
+      required Map configMap}) async {
+    FormData formdata = FormData.fromMap({
+      "file": await MultipartFile.fromFile(path, filename: name),
+      "strategy_id": configMap["strategy_id"],
+    });
+    BaseOptions options = BaseOptions();
+    options.headers = {
+      "Authorization": configMap["token"],
+      "Accept": "application/json",
+      "Content-Type": "multipart/form-data",
+    };
+    Dio dio = Dio(options);
+    String uploadUrl = configMap["host"] + "/api/v1/upload";
+    try {
+      var response = await dio.post(uploadUrl, data: formdata);
+      if (response.statusCode == 200 && response.data!['status'] == true) {
+        return "sucess";
+      } else {
+        return "failed";
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
 }
