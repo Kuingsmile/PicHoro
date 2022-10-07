6f94e783436105b03104b6294f2da38ead123028
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-07 11:51:15 +0800

    增加了文件重命名和复制链接等功能选项

--- lib/pages/commonConfig.dart
+++ lib/pages/commonConfig.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flustars_flutter3/flustars_flutter3.dart';
-import 'package:provider/provider.dart';
-import 'package:horopic/utils/themeProvider.dart';
+import 'package:horopic/utils/global.dart';
 import 'package:horopic/pages/changeTheme.dart';
-import 'package:horopic/pages/APPpassword.dart';
+import 'package:horopic/pages/linkFormatSelect.dart';
 
 class CommonConfig extends StatefulWidget {
   const CommonConfig({Key? key}) : super(key: key);
@@ -14,6 +12,11 @@ class CommonConfig extends StatefulWidget {
 
 class _CommonConfigState extends State<CommonConfig> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -23,6 +26,38 @@ class _CommonConfigState extends State<CommonConfig> {
       body: ListView(
         children: [
           ListTile(
+            title: const Text('是否开启时间戳重命名'),
+            subtitle: const Text('同时开启的话优先时间戳'),
+            trailing: Switch(
+              value: Global.isTimeStamp,
+              onChanged: (value) async {
+                await Global.setTimeStamp(value);
+                setState(() {});
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('是否开启随机字符串重命名'),
+            subtitle: const Text('字符串长度为30'),
+            trailing: Switch(
+              value: Global.isRandomName,
+              onChanged: (value) async {
+                await Global.setRandomName(value);
+                setState(() {});
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('上传后是否自动复制链接'),
+            trailing: Switch(
+              value: Global.isCopyLink,
+              onChanged: (value) async {
+                await Global.setCopyLink(value);
+                setState(() {});
+              },
+            ),
+          ),
+          ListTile(
             title: const Text('主题设置'),
             trailing: const Icon(Icons.arrow_forward_ios),
             onTap: () {
@@ -30,7 +65,16 @@ class _CommonConfigState extends State<CommonConfig> {
                   MaterialPageRoute(builder: (context) => const ChangeTheme()));
             },
           ),
-          
+          ListTile(
+            title: const Text('默认复制链接格式'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const LinkFormatSelect()));
+            },
+          ),
         ],
       ),
     );
