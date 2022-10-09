1346fdb3980c39be13e4910ce9de358679cc873b
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-09 18:03:01 +0800

    相册功能的实现

---
+++ lib/album/EmptyDatabase.dart
@@ -0,0 +1,84 @@
+import 'package:horopic/album/albumSQL.dart';
+import 'package:flutter/material.dart';
+import 'package:horopic/utils/global.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class EmptyDatabase extends StatefulWidget {
+  @override
+  _EmptyDatabaseState createState() => _EmptyDatabaseState();
+}
+
+class _EmptyDatabaseState extends State<EmptyDatabase> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('选择需要清空的数据库'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('兰空'),
+            onTap: () async {
+              await AlbumSQL.DeleteTable(Global.imageDB!, 'lskypro');
+              Fluttertoast.showToast(
+                  msg: "已请空兰空数据库",
+                  toastLength: Toast.LENGTH_SHORT,
+                  timeInSecForIosWeb: 2,
+                  backgroundColor:
+                      Theme.of(context).brightness == Brightness.light
+                          ? Colors.black
+                          : Colors.white,
+                  textColor: Theme.of(context).brightness == Brightness.light
+                      ? Colors.white
+                      : Colors.black,
+                  fontSize: 16.0);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('SM.MS'),
+            onTap: () async {
+              await AlbumSQL.DeleteTable(Global.imageDB!, 'smms');
+              Fluttertoast.showToast(
+                  msg: "已请空SM.MS数据库",
+                  toastLength: Toast.LENGTH_SHORT,
+                  timeInSecForIosWeb: 2,
+                  backgroundColor:
+                      Theme.of(context).brightness == Brightness.light
+                          ? Colors.black
+                          : Colors.white,
+                  textColor: Theme.of(context).brightness == Brightness.light
+                      ? Colors.white
+                      : Colors.black,
+                  fontSize: 16.0);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('所有数据库'),
+            onTap: () async {
+              await AlbumSQL.EmptyAllTable(
+                Global.imageDB!,
+              );
+              Fluttertoast.showToast(
+                  msg: "已请空所有数据库",
+                  toastLength: Toast.LENGTH_SHORT,
+                  timeInSecForIosWeb: 2,
+                  backgroundColor:
+                      Theme.of(context).brightness == Brightness.light
+                          ? Colors.black
+                          : Colors.white,
+                  textColor: Theme.of(context).brightness == Brightness.light
+                      ? Colors.white
+                      : Colors.black,
+                  fontSize: 16.0);
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/album/LoadStateChanged.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:extended_image/extended_image.dart';
+Widget? defaultLoadStateChanged(ExtendedImageState state,
+    {double iconSize = 16}) {
+  switch (state.extendedImageLoadState) {
+    case LoadState.loading:
+      return Center(
+        child: Center(
+          child: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: CircularProgressIndicator(
+              strokeWidth: 2.0,
+            ),
+          ),
+        ),
+      );
+    case LoadState.failed:
+      return GestureDetector(
+        child: Stack(
+          fit: StackFit.expand,
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            Icon(
+              Icons.error,
+              size: iconSize,
+              color: Colors.grey[600],
+            )
+          ],
+        ),
+        onTap: () {
+          state.reLoadImage();
+        },
+      );
+    default:
+      return null;
+  }
+}
