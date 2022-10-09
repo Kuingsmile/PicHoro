3b67f37a455db8b954996addee314d2191e852a3
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-09 18:02:36 +0800

    增加删除功能函数

--- lib/api/smms.dart
+++ lib/api/smms.dart
@@ -4,7 +4,7 @@ import 'package:horopic/utils/global.dart';
 
 class SmmsImageUploadUtils {
   //上传接口
-  uploadApi(
+  static uploadApi(
       {required String path,
       required String name,
       required Map configMap}) async {
@@ -27,13 +27,41 @@ class SmmsImageUploadUtils {
       var response = await dio.post(uploadUrl, data: formdata);
       if (response.statusCode == 200 && response.data!['success'] == true) {
         String returnUrl = response.data!['data']['url'];
+        String pictureKey = response.data!['data']['hash'];
         if (Global.isCopyLink == true) {
           formatedURL =
               linkGenerateDict[Global.defaultLKformat]!(returnUrl, name);
         } else {
           formatedURL = returnUrl;
         }
-        return ["success", formatedURL];
+        return ["success", formatedURL, returnUrl, pictureKey];
+      } else {
+        return ["failed"];
+      }
+    } catch (e) {
+      return [e.toString()];
+    }
+  }
+
+  static deleteApi({required Map deleteMap, required Map configMap}) async {
+    Map<String, dynamic> formdata = {
+      "hash": deleteMap["pictureKey"],
+      "format": "json",
+    };
+
+    BaseOptions options = BaseOptions();
+    options.headers = {
+      "Authorization": configMap["token"],
+    };
+    Dio dio = Dio(options);
+    String uploadUrl =
+        "https://smms.app/api/v2/delete/${deleteMap["pictureKey"]}";
+    //String uploadUrl = "https://sm.ms/api/v2/delete/:hash"; //主要接口,国内访问不了
+
+    try {
+      var response = await dio.get(uploadUrl, queryParameters: formdata);
+      if (response.statusCode == 200 && response.data!['success'] == true) {
+        return ["success"];
       } else {
         return ["failed"];
       }
