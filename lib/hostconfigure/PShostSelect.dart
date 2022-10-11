5c8741093628471c0144ff8ae59855385b1af896
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-11 17:54:44 +0800

    配置默认图床的时候同步修改远程和本地

--- lib/hostconfigure/PShostSelect.dart
+++ lib/hostconfigure/PShostSelect.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:horopic/utils/global.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:horopic/utils/sqlUtils.dart';
 
 class defaultPShostSelect extends StatefulWidget {
   const defaultPShostSelect({Key? key}) : super(key: key);
@@ -34,7 +36,7 @@ class _defaultPShostSelectState extends State<defaultPShostSelect> {
                 ? const Icon(Icons.check)
                 : null,
             onTap: () async {
-              await Global.setPShost('lsky.pro');
+              await setdefaultPShostRemoteAndLocal('lsky.pro');
               setState(() {});
             },
           ),
@@ -44,12 +46,114 @@ class _defaultPShostSelectState extends State<defaultPShostSelect> {
                 ? const Icon(Icons.check)
                 : null,
             onTap: () async {
-              await Global.setPShost('sm.ms');
+              await setdefaultPShostRemoteAndLocal('sm.ms');
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: const Text('Github图床'),
+            trailing: Global.defaultPShost == 'github'
+                ? const Icon(Icons.check)
+                : null,
+            onTap: () async {
+              await setdefaultPShostRemoteAndLocal('github');
               setState(() {});
             },
           ),
         ],
       ),
     );
+  }
+}
+
+setdefaultPShostRemoteAndLocal(String psHost) async {
+  //图床名和查询函数对应字典
+  Map<String, Function> queryFunc = {
+    'lsky.pro': MySqlUtils.queryLankong,
+    'sm.ms': MySqlUtils.querySmms,
+    'github': MySqlUtils.queryGithub,
+  };
+  try {
+    String defaultUser = await Global.getUser();
+    String defaultPassword = await Global.getPassword();
+    var queryuser = await MySqlUtils.queryUser(username: defaultUser);
+
+    if (queryuser == 'Empty') {
+      return Fluttertoast.showToast(
+          msg: "请先注册用户",
+          toastLength: Toast.LENGTH_SHORT,
+          timeInSecForIosWeb: 2,
+          fontSize: 16.0);
+    } else if (queryuser['password'] != defaultPassword) {
+      return Fluttertoast.showToast(
+          msg: "请先登录",
+          toastLength: Toast.LENGTH_SHORT,
+          timeInSecForIosWeb: 2,
+          fontSize: 16.0);
+    }
+    var querypsHost = await queryFunc[psHost]!(username: defaultUser);
+    if (querypsHost == 'Empty') {
+      return Fluttertoast.showToast(
+          msg: "请先配置上传参数",
+          toastLength: Toast.LENGTH_SHORT,
+          timeInSecForIosWeb: 2,
+          fontSize: 16.0);
+    }
+    if (querypsHost == 'Error') {
+      return Fluttertoast.showToast(
+          msg: "Error",
+          toastLength: Toast.LENGTH_SHORT,
+          timeInSecForIosWeb: 2,
+          fontSize: 16.0);
+    }
+    if (queryuser['defaultPShost'] == psHost) {
+      await Global.setPShost(psHost);
+      if (psHost == 'lsky.pro') {
+        await Global.setShowedPBhost('lskypro');
+      } else if (psHost == 'sm.ms') {
+        await Global.setShowedPBhost('smms');
+      } else {
+        await Global.setShowedPBhost(psHost);
+      }
+      return Fluttertoast.showToast(
+          msg: "已经是默认配置",
+          toastLength: Toast.LENGTH_SHORT,
+          timeInSecForIosWeb: 2,
+          fontSize: 16.0);
+    } else {
+      List sqlconfig = [];
+      sqlconfig.add(defaultUser);
+      sqlconfig.add(defaultPassword);
+      sqlconfig.add(psHost);
+      var updateResult = await MySqlUtils.updateUser(content: sqlconfig);
+
+      if (updateResult == 'Success') {
+        await Global.setPShost(psHost);
+        if (psHost == 'lsky.pro') {
+          await Global.setShowedPBhost('lskypro');
+        } else if (psHost == 'sm.ms') {
+          await Global.setShowedPBhost('smms');
+        } else {
+          await Global.setShowedPBhost(psHost);
+        }
+        Fluttertoast.showToast(
+            msg: "已设置$psHost为默认图床",
+            toastLength: Toast.LENGTH_SHORT,
+            timeInSecForIosWeb: 2,
+            fontSize: 16.0);
+      } else {
+        Fluttertoast.showToast(
+            msg: "写入数据库失败",
+            toastLength: Toast.LENGTH_SHORT,
+            timeInSecForIosWeb: 2,
+            fontSize: 16.0);
+      }
+    }
+  } catch (e) {
+    Fluttertoast.showToast(
+        msg: "Error",
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 2,
+        fontSize: 16.0);
   }
 }
