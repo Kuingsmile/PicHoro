47b77922d42bb7a72b44d452a9a3437b50ae06df
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-13 17:42:49 +0800

    增加七牛云支持

--- lib/configurePage/commonConfigure/selectPShost.dart
+++ lib/configurePage/commonConfigure/selectPShost.dart
@@ -15,6 +15,10 @@ import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:horopic/hostconfigure/Imgurconfig.dart';
 import 'package:dio_proxy_adapter/dio_proxy_adapter.dart';
+import 'package:horopic/hostconfigure/qiniuconfig.dart';
+import 'package:horopic/api/qiniu.dart';
+import 'package:flutter/services.dart';
+import 'package:qiniu_flutter_sdk/qiniu_flutter_sdk.dart';
 
 //a configure page for user to show configure entry
 class AllPShost extends StatefulWidget {
@@ -88,13 +92,21 @@ class _AllPShostState extends State<AllPShost> {
     return File('$path/${defaultUser}_imgur_config.txt');
   }
 
+  //qiniu配置
+  Future<File> get qiniuFile async {
+    final path = await _localPath;
+    String defaultUser = await Global.getUser();
+    return File('$path/${defaultUser}_qiniu_config.txt');
+  }
+
   processingQRCodeResult() async {
     String result = Global.qrScanResult;
     Global.qrScanResult = "";
     if (!(result.contains('smms')) &&
         !(result.contains('github')) &&
         !(result.contains('lankong')) &&
-        !(result.contains('imgur'))) {
+        !(result.contains('imgur')) &&
+        !(result.contains('qiniu'))) {
       return Fluttertoast.showToast(
           msg: "不包含支持的图床配置信息",
           toastLength: Toast.LENGTH_SHORT,
@@ -123,9 +135,10 @@ class _AllPShostState extends State<AllPShost> {
         // String validateURL = "https://sm.ms/api/v2/profile";被墙了
         BaseOptions options = BaseOptions(
           //连接服务器超时时间，单位是毫秒.
-          connectTimeout: 10000,
+          connectTimeout: 30000,
           //响应超时时间。
-          receiveTimeout: 10000,
+          receiveTimeout: 30000,
+          sendTimeout: 30000,
         );
         options.headers = {
           "Content-Type": 'multipart/form-data',
@@ -232,9 +245,10 @@ class _AllPShostState extends State<AllPShost> {
           }
           BaseOptions options = BaseOptions(
             //连接服务器超时时间，单位是毫秒.
-            connectTimeout: 10000,
+            connectTimeout: 30000,
             //响应超时时间。
-            receiveTimeout: 10000,
+            receiveTimeout: 30000,
+            sendTimeout: 30000,
           );
           options.headers = {
             "Accept": 'application/vnd.github+json',
@@ -268,7 +282,7 @@ class _AllPShostState extends State<AllPShost> {
                     fontSize: 16.0);
               } else {
                 Fluttertoast.showToast(
-                    msg: "github数据库错误",
+                    msg: "Github数据库错误",
                     toastLength: Toast.LENGTH_SHORT,
                     timeInSecForIosWeb: 2,
                     fontSize: 16.0);
@@ -325,9 +339,10 @@ class _AllPShostState extends State<AllPShost> {
 
             BaseOptions options = BaseOptions(
               //连接服务器超时时间，单位是毫秒.
-              connectTimeout: 10000,
+              connectTimeout: 30000,
               //响应超时时间。
-              receiveTimeout: 10000,
+              receiveTimeout: 30000,
+              sendTimeout: 30000,
             );
             options.headers = {
               "Accept": "application/json",
@@ -452,9 +467,10 @@ class _AllPShostState extends State<AllPShost> {
 
         BaseOptions options = BaseOptions(
           //连接服务器超时时间，单位是毫秒.
-          connectTimeout: 10000,
+          connectTimeout: 30000,
           //响应超时时间。
-          receiveTimeout: 10000,
+          receiveTimeout: 30000,
+          sendTimeout: 30000,
         );
         options.headers = {
           "Authorization": "Client-ID $imgurclientId",
@@ -516,6 +532,125 @@ class _AllPShostState extends State<AllPShost> {
       } catch (e) {
         Fluttertoast.showToast(
             msg: "Imgur配置错误",
+            toastLength: Toast.LENGTH_SHORT,
+            timeInSecForIosWeb: 2,
+            fontSize: 16.0);
+      }
+    }
+
+    if (jsonResult['qiniu'] != null) {
+      String qiniuAccessKey = jsonResult['qiniu']['accessKey'];
+      String qiniuSecretKey = jsonResult['qiniu']['secretKey'];
+      String qiniuBucket = jsonResult['qiniu']['bucket'];
+      String qiniuUrl = jsonResult['qiniu']['url'];
+      String qiniuArea = jsonResult['qiniu']['area'];
+      String qiniuOptions = jsonResult['qiniu']['options'];
+      String qiniuPath = jsonResult['qiniu']['path'];
+
+      try {
+        List sqlconfig = [];
+        sqlconfig.add(qiniuAccessKey);
+        sqlconfig.add(qiniuSecretKey);
+        sqlconfig.add(qiniuBucket);
+        sqlconfig.add(qiniuUrl);
+        sqlconfig.add(qiniuArea);
+        sqlconfig.add(qiniuOptions);
+        sqlconfig.add(qiniuPath);
+        String defaultUser = await Global.getUser();
+        sqlconfig.add(defaultUser);
+
+        var queryqiniu = await MySqlUtils.queryQiniu(username: defaultUser);
+        var queryuser = await MySqlUtils.queryUser(username: defaultUser);
+
+        if (queryuser == 'Empty') {
+          Fluttertoast.showToast(
+              msg: "请先登录",
+              toastLength: Toast.LENGTH_SHORT,
+              timeInSecForIosWeb: 2,
+              fontSize: 16.0);
+        }
+        if (!qiniuUrl.startsWith('http') && !qiniuUrl.startsWith('https')) {
+          qiniuUrl = 'http://$qiniuUrl';
+        }
+
+        if (qiniuPath.startsWith('/')) {
+          qiniuPath = qiniuPath.substring(1);
+        }
+        if (!qiniuPath.endsWith('/')) {
+          qiniuPath = '$qiniuPath/';
+        }
+        //save asset image to app dir
+        String assetPath = 'assets/validateImage/PicHoroValidate.jpeg';
+        String appDir = await getApplicationDocumentsDirectory().then((value) {
+          return value.path;
+        });
+        String assetFilePath = '$appDir/PicHoroValidate.jpeg';
+        File assetFile = File(assetFilePath);
+
+        if (!assetFile.existsSync()) {
+          ByteData data = await rootBundle.load(assetPath);
+          List<int> bytes =
+              data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+          await assetFile.writeAsBytes(bytes);
+        }
+        String key = 'PicHoroValidate.jpeg';
+        String urlSafeBase64EncodePutPolicy =
+            QiniuImageUploadUtils.geturlSafeBase64EncodePutPolicy(
+                qiniuBucket, key, qiniuPath);
+        String uploadToken = QiniuImageUploadUtils.getUploadToken(
+            qiniuAccessKey, qiniuSecretKey, urlSafeBase64EncodePutPolicy);
+        Storage storage = Storage(
+            config: Config(
+          retryLimit: 5,
+        ));
+
+        String sqlResult = '';
+        try {
+          PutResponse putresult =
+              await storage.putFile(File(assetFilePath), uploadToken);
+          if (putresult.key == key || putresult.key == '$qiniuPath$key') {
+            if (queryqiniu == 'Empty') {
+              sqlResult = await MySqlUtils.insertQiniu(content: sqlconfig);
+            } else {
+              sqlResult = await MySqlUtils.updateQiniu(content: sqlconfig);
+            }
+            if (sqlResult == "Success") {
+              final qiniuConfig = QiniuConfigModel(
+                  qiniuAccessKey,
+                  qiniuSecretKey,
+                  qiniuBucket,
+                  qiniuUrl,
+                  qiniuArea,
+                  qiniuOptions,
+                  qiniuPath);
+              final qiniuConfigJson = jsonEncode(qiniuConfig);
+              final qiniuConfigFile = await qiniuFile;
+              await qiniuConfigFile.writeAsString(qiniuConfigJson);
+              Fluttertoast.showToast(
+                  msg: "七牛云配置成功",
+                  toastLength: Toast.LENGTH_SHORT,
+                  timeInSecForIosWeb: 2,
+                  fontSize: 16.0);
+            } else {
+              Fluttertoast.showToast(
+                  msg: "七牛云数据库错误",
+                  toastLength: Toast.LENGTH_SHORT,
+                  timeInSecForIosWeb: 2,
+                  fontSize: 16.0);
+            }
+          } else {
+            Fluttertoast.showToast(
+                msg: "七牛云验证失败",
+                toastLength: Toast.LENGTH_SHORT,
+                timeInSecForIosWeb: 2,
+                fontSize: 16.0);
+          }
+        } catch (e) {
+          rethrow;
+        }
+      } catch (e) {
+        Fluttertoast.showToast(
+            msg: "七牛云配置错误",
             toastLength: Toast.LENGTH_SHORT,
             timeInSecForIosWeb: 2,
             fontSize: 16.0);
@@ -605,6 +740,14 @@ class _AllPShostState extends State<AllPShost> {
             onTap: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const ImgurConfig()));
+            },
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          ListTile(
+            title: const Text('七牛云'),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const QiniuConfig()));
             },
             trailing: const Icon(Icons.arrow_forward_ios),
           ),
