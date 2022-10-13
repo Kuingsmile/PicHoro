9517f30692b2f20603fed17085f6c9d1bafafd43
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-13 17:41:50 +0800

    修改超时时间

--- lib/api/smms.dart
+++ lib/api/smms.dart
@@ -16,9 +16,10 @@ class SmmsImageUploadUtils {
 
     BaseOptions options = BaseOptions(
       //连接服务器超时时间，单位是毫秒.
-      connectTimeout: 10000,
+      connectTimeout: 30000,
       //响应超时时间。
-      receiveTimeout: 10000,
+      receiveTimeout: 30000,
+      sendTimeout: 30000,
     );
     options.headers = {
       "Authorization": configMap["token"],
@@ -56,9 +57,10 @@ class SmmsImageUploadUtils {
 
     BaseOptions options = BaseOptions(
       //连接服务器超时时间，单位是毫秒.
-      connectTimeout: 10000,
+      connectTimeout: 30000,
       //响应超时时间。
-      receiveTimeout: 10000,
+      receiveTimeout: 30000,
+      sendTimeout: 30000,
     );
     options.headers = {
       "Authorization": configMap["token"],
