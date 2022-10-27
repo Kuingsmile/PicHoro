987a7cf27cf38ca08a10d18e7e9ef1af0e5f8514
萌萌哒赫萝 <ma_shiqing@163.com>
2022-10-27 11:14:52 +0800

    变更：修改文件名

---
+++ lib/PShostFileManage/tencent/tencent_bucket_information_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:horopic/picture_host_manage/manage_api/tencent_manage_api.dart';
+
+class BucketInformation extends StatefulWidget {
+  final Map bucketMap;
+  const BucketInformation({Key? key, required this.bucketMap})
+      : super(key: key);
+
+  @override
+  BucketInformationState createState() => BucketInformationState();
+}
+
+class BucketInformationState extends State<BucketInformation> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('基本信息'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('存储桶名称'),
+            subtitle: Text(widget.bucketMap['name']),
+          ),
+          ListTile(
+            title: const Text('所属地域'),
+            subtitle: Text(
+                '${widget.bucketMap['location']}(${TencentManageAPI.areaCodeName[widget.bucketMap['location']]})'),
+          ),
+          ListTile(
+            title: const Text('创建时间'),
+            subtitle: Text(widget.bucketMap['CreationDate']),
+          ),
+          ListTile(
+            isThreeLine: true,
+            title: const Text('访问域名'),
+            subtitle: Text(
+                'https://${widget.bucketMap['name']}.cos.${widget.bucketMap['location']}.myqcloud.com'),
+          ),
+        ],
+      ),
+    );
+  }
+}
