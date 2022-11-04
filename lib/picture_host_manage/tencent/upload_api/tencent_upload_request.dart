93e75e9fefc6f83804e899deba4333dba29bc9ae
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-04 21:16:18 +0800

    新增：上传管理功能相关代码

---
+++ lib/picture_host_manage/tencent/upload_api/tencent_upload_request.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+
+class UploadRequest {
+  final String path;
+  final String name;
+  final Map<String, dynamic> configMap;
+  var cancelToken = CancelToken();
+
+  UploadRequest(
+    this.path,
+    this.name,
+    this.configMap,
+  );
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UploadRequest &&
+          runtimeType == other.runtimeType &&
+          path == other.path &&
+          name == other.name &&
+          configMap == other.configMap;
+
+  @override
+  int get hashCode => path.hashCode ^ name.hashCode ^ configMap.hashCode;
+}
