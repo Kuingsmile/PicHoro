edd12ae275b5c28a00f4f0a6e53774e273ede137
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-16 13:42:40 +0800

    新增：markdown文件查看页面

---
+++ lib/picture_host_manage/common_page/file_explorer/md_preview.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:horopic/album/load_state_change.dart';
+
+class MarkDownPreview extends StatefulWidget {
+  final String filePath;
+  final String fileName;
+  const MarkDownPreview({Key? key,
+    required this.filePath,
+    required this.fileName,
+  }) : super(key: key);
+
+  @override
+  MarkDownPreviewState createState() => MarkDownPreviewState();
+}
+
+class MarkDownPreviewState extends State<MarkDownPreview> {
+  late Future<String> _future;
+
+  @override
+  void initState() {
+    super.initState();
+    _future = File(widget.filePath).readAsString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title:  Text(widget.fileName),
+      ),
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Markdown(
+              data: snapshot.data!,
+              selectable: true,
+              imageBuilder: (uri, title, alt) {
+                return ExtendedImage.network(
+                  uri.toString(),
+                  fit: BoxFit.contain,
+                  mode: ExtendedImageMode.gesture,
+                  cache: true,
+                  loadStateChanged: (state) =>
+                      defaultLoadStateChanged(state, iconSize: 60),
+                  initGestureConfigHandler: (state) {
+                    return GestureConfig(
+                        minScale: 0.9,
+                        animationMinScale: 0.7,
+                        maxScale: 3.0,
+                        animationMaxScale: 3.5,
+                        speed: 1.0,
+                        inertialSpeed: 100.0,
+                        initialScale: 1.0,
+                        inPageView: true);
+                  },
+                );
+              },
+              onTapLink: (text, href, title) async {
+                Uri url = Uri.parse(href!);
+                await launchUrl(url);
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
