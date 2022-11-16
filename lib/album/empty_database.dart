9cf97d94ea4a0717661b22b4d6d7ab34d444433c
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-16 13:52:06 +0800

    新增：增加FTP数据库清空相关代码

--- lib/album/empty_database.dart
+++ lib/album/empty_database.dart
@@ -58,6 +58,22 @@ class EmptyDatabaseState extends State<EmptyDatabase> {
     for (int i = 0; i < _psHostNameList.length; i++) {
       listTiles.add(getListTile(context, i));
     }
+    ListTile ftpEmpty = ListTile(
+        title: const Center(child: Text('FTP')),
+        onTap: () async {
+          showCupertinoAlertDialogWithConfirmFunc(
+            title: '通知',
+            content: '是否确定清空FTP数据库？',
+            context: context,
+            onConfirm: () async {
+              Navigator.pop(context);
+              await AlbumSQL.deleteTable(
+                  Global.imageDBExtend!, 'PBhostExtend1');
+              showToast('已清空FTP数据库');
+              eventBus.fire(AlbumRefreshEvent(albumKeepAlive: false));
+            },
+          );
+        });
     ListTile allEmpty = ListTile(
         title: const Center(child: Text('所有数据库')),
         onTap: () async {
@@ -70,11 +86,15 @@ class EmptyDatabaseState extends State<EmptyDatabase> {
               await AlbumSQL.emptyAllTable(
                 Global.imageDB!,
               );
+              await AlbumSQL.emptyAllTableExtend(
+                Global.imageDBExtend!,
+              );
               showToast('已清空所有数据库');
               eventBus.fire(AlbumRefreshEvent(albumKeepAlive: false));
             },
           );
         });
+    listTiles.add(ftpEmpty);
     listTiles.add(allEmpty);
     return listTiles;
   }
