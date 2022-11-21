171ca93c33f605732d5e791faa54b87e323c08e8
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-21 14:01:56 +0800

    新增：修改为Extended image预览

--- lib/picture_host_manage/common_page/file_explorer/local_image_preview.dart
+++ lib/picture_host_manage/common_page/file_explorer/local_image_preview.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:f_logs/f_logs.dart';
 import 'package:extended_image/extended_image.dart';
 
+import 'package:horopic/album/load_state_change.dart';
 import 'package:horopic/utils/common_functions.dart';
 
 class LocalImagePreview extends StatefulWidget {
@@ -46,22 +47,44 @@ class LocalImagePreviewState extends State<LocalImagePreview> {
         physics: const BouncingScrollPhysics(),
         itemBuilder: (context, index) {
           try {
-            return Image.file(
-              File(widget.images[index]),
-              fit: BoxFit.contain,
-              //mode: ExtendedImageMode.gesture,
-              /* initGestureConfigHandler: (state) {
-                return GestureConfig(
-                    minScale: 0.9,
-                    animationMinScale: 0.7,
-                    maxScale: 3.0,
-                    animationMaxScale: 3.5,
-                    speed: 1.0,
-                    inertialSpeed: 100.0,
-                    initialScale: 1.0,
-                    inPageView: true);
-              },*/
-            );
+            if (File(widget.images[index]).existsSync()) {
+              return ExtendedImage.file(
+                File(widget.images[index]),
+                fit: BoxFit.contain,
+                mode: ExtendedImageMode.gesture,
+                clearMemoryCacheIfFailed: true,
+                loadStateChanged: (state) =>
+                    defaultLoadStateChanged(state, iconSize: 60),
+                initGestureConfigHandler: (state) {
+                  return GestureConfig(
+                      minScale: 0.9,
+                      animationMinScale: 0.7,
+                      maxScale: 3.0,
+                      animationMaxScale: 3.5,
+                      speed: 1.0,
+                      inertialSpeed: 100.0,
+                      initialScale: 1.0,
+                      inPageView: true);
+                },
+              );
+            } else {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/empty.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                    const Text('文件不存在',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color.fromARGB(136, 121, 118, 118)))
+                  ],
+                ),
+              );
+            }
           } catch (e) {
             FLog.error(
                 className: 'LocalImagePreviewState',
