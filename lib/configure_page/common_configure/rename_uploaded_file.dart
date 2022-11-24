f43d0b82197910b1d8c632959dd0671e30b053b6
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-24 15:13:07 +0800

    维护：精简代码

--- lib/configure_page/common_configure/rename_uploaded_file.dart
+++ lib/configure_page/common_configure/rename_uploaded_file.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:horopic/utils/global.dart';
+import 'package:horopic/utils/common_functions.dart';
 
 class RenameFile extends StatefulWidget {
   const RenameFile({Key? key}) : super(key: key);
@@ -9,23 +10,78 @@ class RenameFile extends StatefulWidget {
 }
 
 class RenameFileState extends State<RenameFile> {
+  Map placeholderMap = {
+    '{Y}': "年份(2022)",
+    '{y}': "两位数年份(22)",
+    '{m}': "月份(01-12)",
+    '{d}': "日期(01-31)",
+    '{timestamp}': "时间戳(秒)",
+    '{uuid}': "唯一字符串",
+    '{md5}': "随机md5",
+    '{md5-16}': "随机md5前16位",
+    '{str-10}': "随机10位字符串",
+    '{str-20}': "随机20位字符串",
+    '{filename}': "原文件名",
+  };
+
   @override
   void initState() {
     super.initState();
+  }
+
+  TableRow _buildTableRow(String key, String value) {
+    return TableRow(
+      children: [
+        TableCell(child: Center(child: Text(key))),
+        TableCell(child: Center(child: Text(value))),
+      ],
+    );
+  }
+
+  List<TableRow> _buildTableRows() {
+    List<TableRow> rows = [];
+    placeholderMap.forEach((key, value) {
+      rows.add(_buildTableRow(key, value));
+    });
+    return rows;
+  }
+
+  List<TableRow> _buildTable() {
+    List<TableRow> temp = [
+      const TableRow(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(5),
+              topRight: Radius.circular(5),
+            ),
+          ),
+          color: Colors.grey,
+        ),
+        children: [
+          TableCell(child: Center(child: Text("占位符"))),
+          TableCell(child: Center(child: Text("说明"))),
+        ],
+      )
+    ];
+    temp.addAll(_buildTableRows());
+    return temp;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         elevation: 0,
+        elevation: 0,
         centerTitle: true,
-        title: const Text('文件重命名'),
+        title: titleText(
+          '文件重命名',
+        ),
       ),
       body: ListView(children: [
         ListTile(
           title: const Text('是否开启时间戳重命名'),
-          subtitle: const Text('优先级按照自定义>时间戳>随机字符串'),
+          subtitle: const Text('优先级:自定义>时间戳>随机字符串'),
           trailing: Switch(
             value: Global.isTimeStamp,
             onChanged: (value) async {
@@ -36,7 +92,7 @@ class RenameFileState extends State<RenameFile> {
         ),
         ListTile(
           title: const Text('是否开启随机字符串重命名'),
-          subtitle: const Text('字符串长度固定为30'),
+          subtitle: const Text('固定30位字符串'),
           trailing: Switch(
             value: Global.isRandomName,
             onChanged: (value) async {
@@ -80,89 +136,7 @@ class RenameFileState extends State<RenameFile> {
                 style: BorderStyle.solid,
                 borderRadius: BorderRadius.circular(5),
               ),
-              children: const [
-                TableRow(
-                  decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                      ),
-                    ),
-                    color: Colors.grey,
-                  ),
-                  children: [
-                    TableCell(child: Center(child: Text("占位符"))),
-                    TableCell(child: Center(child: Text("说明"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{Y}"))),
-                    TableCell(child: Center(child: Text("年份(2022)"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{y}"))),
-                    TableCell(child: Center(child: Text("两位数年份(22)"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{m}"))),
-                    TableCell(child: Center(child: Text("月份(01-12)"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{d}"))),
-                    TableCell(child: Center(child: Text("日期(01-31)"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{timestamp}"))),
-                    TableCell(child: Center(child: Text("时间戳(秒)"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{uuid}"))),
-                    TableCell(child: Center(child: Text("唯一字符串"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{md5}"))),
-                    TableCell(child: Center(child: Text("随机md5"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{md5-16}"))),
-                    TableCell(child: Center(child: Text("随机md5前16位"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{str-10}"))),
-                    TableCell(child: Center(child: Text("10位随机字符串"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{str-20}"))),
-                    TableCell(child: Center(child: Text("20位随机字符串"))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text("{filename}"))),
-                    TableCell(child: Center(child: Text("原始文件名"))),
-                  ],
-                ),
-              ],
+              children: _buildTable(),
             )),
       ]),
     );
