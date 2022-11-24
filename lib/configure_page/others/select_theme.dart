bda6d45d774461a271bd5af9ba03c2d1f347073d
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-24 15:12:19 +0800

    新增：新增主题选择

--- lib/configure_page/others/select_theme.dart
+++ lib/configure_page/others/select_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:horopic/utils/theme_provider.dart';
+import 'package:horopic/utils/common_functions.dart';
 
 class ChangeTheme extends StatefulWidget {
   const ChangeTheme({Key? key}) : super(key: key);
@@ -13,10 +14,8 @@ class ChangeThemeState extends State<ChangeTheme> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('主题设置'),
-        ),
+        appBar:
+            AppBar(elevation: 0, centerTitle: true, title: titleText('主题设置')),
         body: Consumer<AppInfoProvider>(builder: (context, appinfo, child) {
           return ListView(
             children: [
@@ -47,6 +46,107 @@ class ChangeThemeState extends State<ChangeTheme> {
                   appinfo.setTheme('dark');
                 },
               ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 30,
+                    height: 40,
+                    child: Container(
+                      color: Colors.green,
+                      child: TextButton(
+                        onPressed: () {
+                          appinfo.setTheme('green');
+                        },
+                        child: const Text(''),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 30,
+                    height: 40,
+                    child: Container(
+                      color: Colors.purple,
+                      child: GestureDetector(
+                        onTap: () {
+                          appinfo.setTheme('purple');
+                        },
+                        child: const Text(''),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 30,
+                    height: 40,
+                    child: Container(
+                      color: Colors.orange,
+                      child: GestureDetector(
+                        onTap: () {
+                          appinfo.setTheme('orange');
+                        },
+                        child: const Text(''),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 30,
+                    height: 40,
+                    child: Container(
+                      color: const Color.fromARGB(255, 241, 160, 187),
+                      child: GestureDetector(
+                        onTap: () {
+                          appinfo.setTheme('pink');
+                        },
+                        child: const Text(''),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 30,
+                    height: 40,
+                    child: Container(
+                      color: Colors.cyan,
+                      child: GestureDetector(
+                        onTap: () {
+                          appinfo.setTheme('cyan');
+                        },
+                        child: const Text(''),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 30,
+                    height: 40,
+                    child: Container(
+                      color: const Color.fromARGB(255, 255, 215, 0),
+                      child: GestureDetector(
+                        onTap: () {
+                          appinfo.setTheme('gold');
+                        },
+                        child: const Text(''),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           );
         }));
