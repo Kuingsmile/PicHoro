d09453e53e63e2bcc150c2ca160049a07ee6263f
萌萌哒赫萝 <ma_shiqing@163.com>
2022-11-25 15:31:36 +0800

    修复：全部上传按钮会重新上传已完成任务的问题

--- lib/picture_host_manage/upyun/upyun_download_manage_page.dart
+++ lib/picture_host_manage/upyun/upyun_download_manage_page.dart
@@ -63,7 +63,7 @@ class UpyunUpDownloadManagePageState extends State<UpyunUpDownloadManagePage> {
 
   _createUploadListItem() {
     List<Widget> list = [];
-    for (var i = 0; i < Global.upyunUploadList.length; i++) {
+    for (var i =  Global.upyunUploadList.length - 1; i >= 0; i--) {
       list.add(GestureDetector(
           onLongPress: () {
             showCupertinoAlertDialogWithConfirmFunc(
@@ -88,6 +88,8 @@ class UpyunUpDownloadManagePageState extends State<UpyunUpDownloadManagePage> {
                       break;
                     case UploadStatus.paused:
                       await uploadManager.resumeUpload(path, fileName);
+                      break;
+                    default:
                       break;
                   }
                   setState(() {});
@@ -174,7 +176,7 @@ class UpyunUpDownloadManagePageState extends State<UpyunUpDownloadManagePage> {
 
   _createDownloadListItem() {
     List<Widget> list = [];
-    for (var i = 0; i < Global.upyunDownloadList.length; i++) {
+    for (var i =  Global.upyunDownloadList.length - 1; i >= 0; i--) {
       list.add(GestureDetector(
           onLongPress: () {
             showCupertinoAlertDialogWithConfirmFunc(
@@ -199,6 +201,8 @@ class UpyunUpDownloadManagePageState extends State<UpyunUpDownloadManagePage> {
                       break;
                     case DownloadStatus.paused:
                       await downloadManager.resumeDownload(url);
+                      break;
+                    default:
                       break;
                   }
                   setState(() {});
@@ -365,11 +369,9 @@ class UpyunUpDownloadManagePageState extends State<UpyunUpDownloadManagePage> {
             appBar: AppBar(
               centerTitle: true,
               elevation: 0,
-              title: const Text('上传下载管理',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
+              title: titleText(
+                '上传下载管理',
+              ),
               bottom: const TabBar(
                 padding: EdgeInsets.all(0),
                 indicatorColor: Colors.amber,
@@ -511,9 +513,12 @@ class ListItemState extends State<ListItem> {
                                     Icons.download,
                                     color: Colors.blue,
                                   ));
+                            case DownloadStatus.queued:
+                              return const Icon(
+                                Icons.query_builder_rounded,
+                                color: Colors.blue,
+                              );
                           }
-                          return Text("${downloadStatus[value.toString()]}",
-                              style: const TextStyle(fontSize: 16));
                         })
                     : IconButton(
                         onPressed: () async {
@@ -541,23 +546,6 @@ class ListItemState extends State<ListItem> {
                       ),
                     );
                   }),
-            if (widget.downloadTask != null)
-              FutureBuilder<DownloadStatus>(
-                  future: widget.downloadTask!.whenDownloadComplete(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<DownloadStatus> snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return const Text('请等待下载完成');
-                      default:
-                        if (snapshot.hasError) {
-                          return Text('错误: ${snapshot.error}');
-                        } else {
-                          return Text(
-                              '结果: ${downloadStatus[snapshot.data.toString()]}');
-                        }
-                    }
-                  })
           ],
         ),
       ),
@@ -634,29 +622,6 @@ class UploadListItemState extends State<UploadListItem> {
                         valueListenable: widget.uploadTask!.status,
                         builder: (context, value, child) {
                           switch (widget.uploadTask!.status.value) {
-                            case UploadStatus.uploading:
-                              return IconButton(
-                                  onPressed: () async {
-                                    await widget.onUploadPlayPausedPressed(
-                                        widget.path,
-                                        widget.fileName,
-                                        widget.configMap);
-                                  },
-                                  icon: const Icon(
-                                    Icons.pause,
-                                    color: Colors.blue,
-                                  ));
-                            case UploadStatus.paused:
-                              return IconButton(
-                                onPressed: () async {
-                                  await widget.onUploadPlayPausedPressed(
-                                      widget.path,
-                                      widget.fileName,
-                                      widget.configMap);
-                                },
-                                icon: const Icon(Icons.play_arrow),
-                                color: Colors.blue,
-                              );
                             case UploadStatus.completed:
                               return IconButton(
                                   onPressed: () {
@@ -680,9 +645,35 @@ class UploadListItemState extends State<UploadListItem> {
                                     Icons.cloud_upload_outlined,
                                     color: Colors.blue,
                                   ));
+                            default:
+                              return widget.uploadTask == null ||
+                                      widget.uploadTask!.status.value ==
+                                          UploadStatus.queued
+                                  ? const Icon(
+                                      Icons.query_builder_rounded,
+                                      color: Colors.blue,
+                                    )
+                                  : ValueListenableBuilder(
+                                      valueListenable:
+                                          widget.uploadTask!.progress,
+                                      builder: (context, value, child) {
+                                        return Container(
+                                          height: 20,
+                                          width: 20,
+                                          margin: const EdgeInsets.fromLTRB(
+                                              0, 0, 10, 0),
+                                          child: CircularProgressIndicator(
+                                            value: value,
+                                            strokeWidth: 4,
+                                            color: widget.uploadTask!.status
+                                                        .value ==
+                                                    UploadStatus.paused
+                                                ? Colors.grey
+                                                : Colors.blue,
+                                          ),
+                                        );
+                                      });
                           }
-                          return Text("${uploadStatus[value.toString()]}",
-                              style: const TextStyle(fontSize: 16));
                         })
                     : IconButton(
                         onPressed: () async {
@@ -695,39 +686,6 @@ class UploadListItemState extends State<UploadListItem> {
                         ))
               ],
             ),
-            if (widget.uploadTask != null &&
-                !widget.uploadTask!.status.value.isCompleted)
-              ValueListenableBuilder(
-                  valueListenable: widget.uploadTask!.progress,
-                  builder: (context, value, child) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(vertical: 4),
-                      child: LinearProgressIndicator(
-                        value: value,
-                        color: widget.uploadTask!.status.value ==
-                                UploadStatus.paused
-                            ? Colors.grey
-                            : Colors.amber,
-                      ),
-                    );
-                  }),
-            if (widget.uploadTask != null)
-              FutureBuilder<UploadStatus>(
-                  future: widget.uploadTask!.whenUploadComplete(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<UploadStatus> snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return const Text('请等待上传完成');
-                      default:
-                        if (snapshot.hasError) {
-                          return Text('错误: ${snapshot.error}');
-                        } else {
-                          return Text(
-                              '结果: ${uploadStatus[snapshot.data.toString()]}');
-                        }
-                    }
-                  })
           ],
         ),
       ),
