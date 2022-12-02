950bb9304d3857643d1442a49dd3413c574f77e4
萌萌哒赫萝 <ma_shiqing@163.com>
2022-12-02 15:15:54 +0800

    新增：其它文本字符串显示

--- lib/picture_host_manage/common_page/file_explorer/md_preview.dart
+++ lib/picture_host_manage/common_page/file_explorer/md_preview.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+// ignore: depend_on_referenced_packages
 import 'package:url_launcher/url_launcher.dart';
 import 'package:extended_image/extended_image.dart';
+import 'package:flutter_text_viewer/flutter_text_viewer.dart';
+
 import 'package:horopic/album/load_state_change.dart';
+import 'package:horopic/utils/common_functions.dart';
 
 class MarkDownPreview extends StatefulWidget {
   final String filePath;
   final String fileName;
-  const MarkDownPreview({Key? key,
+  const MarkDownPreview({
+    Key? key,
     required this.filePath,
     required this.fileName,
   }) : super(key: key);
@@ -23,7 +28,9 @@ class MarkDownPreviewState extends State<MarkDownPreview> {
   @override
   void initState() {
     super.initState();
-    _future = File(widget.filePath).readAsString();
+    if (widget.filePath.split(".").last == "md") {
+      _future = File(widget.filePath).readAsString();
+    }
   }
 
   @override
@@ -32,48 +39,59 @@ class MarkDownPreviewState extends State<MarkDownPreview> {
       appBar: AppBar(
         elevation: 0,
         centerTitle: true,
-        title:  Text(widget.fileName),
+        title: titleText(widget.fileName),
       ),
-      body: FutureBuilder(
-        future: _future,
-        builder: (context, AsyncSnapshot<String> snapshot) {
-          if (snapshot.hasData) {
-            return Markdown(
-              data: snapshot.data!,
-              selectable: true,
-              imageBuilder: (uri, title, alt) {
-                return ExtendedImage.network(
-                  uri.toString(),
-                  fit: BoxFit.contain,
-                  mode: ExtendedImageMode.gesture,
-                  cache: true,
-                  loadStateChanged: (state) =>
-                      defaultLoadStateChanged(state, iconSize: 60),
-                  initGestureConfigHandler: (state) {
-                    return GestureConfig(
-                        minScale: 0.9,
-                        animationMinScale: 0.7,
-                        maxScale: 3.0,
-                        animationMaxScale: 3.5,
-                        speed: 1.0,
-                        inertialSpeed: 100.0,
-                        initialScale: 1.0,
-                        inPageView: true);
-                  },
-                );
+      body: widget.filePath.split(".").last == "md"
+          ? FutureBuilder(
+              future: _future,
+              builder: (context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  return Markdown(
+                    data: snapshot.data!,
+                    selectable: true,
+                    imageBuilder: (uri, title, alt) {
+                      return ExtendedImage.network(
+                        uri.toString(),
+                        fit: BoxFit.contain,
+                        mode: ExtendedImageMode.gesture,
+                        cache: true,
+                        loadStateChanged: (state) =>
+                            defaultLoadStateChanged(state, iconSize: 60),
+                        initGestureConfigHandler: (state) {
+                          return GestureConfig(
+                              minScale: 0.9,
+                              animationMinScale: 0.7,
+                              maxScale: 3.0,
+                              animationMaxScale: 3.5,
+                              speed: 1.0,
+                              inertialSpeed: 100.0,
+                              initialScale: 1.0,
+                              inPageView: true);
+                        },
+                      );
+                    },
+                    onTapLink: (text, href, title) async {
+                      Uri url = Uri.parse(href!);
+                      await launchUrl(url);
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
               },
-              onTapLink: (text, href, title) async {
-                Uri url = Uri.parse(href!);
-                await launchUrl(url);
-              },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
+            )
+          : TextViewerPage(
+              textViewer: TextViewer.file(
+                widget.filePath,
+                textStyle: const TextStyle(
+                  fontFamily: 'monospace',
+                  fontSize: 12.0,
+                  height: 1.4,
+                ),
+              ),
+            ),
     );
   }
 }
