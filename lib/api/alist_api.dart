d3acfa9f4d3316cec2c5ac4ba01faf9db37e5643
萌萌哒赫萝 <ma_shiqing@163.com>
2022-12-02 15:28:35 +0800

    新增：Alist上传和删除API

---
+++ lib/api/alist_api.dart
@@ -0,0 +1,297 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:f_logs/f_logs.dart';
+
+import 'package:horopic/picture_host_configure/configure_page/alist_configure.dart';
+import 'package:horopic/utils/common_functions.dart';
+import 'package:horopic/utils/global.dart';
+import 'package:horopic/utils/sql_utils.dart';
+import 'package:horopic/picture_host_manage/manage_api/alist_manage_api.dart';
+
+class AlistImageUploadUtils {
+  //上传接口
+  static uploadApi(
+      {required String path,
+      required String name,
+      required Map configMap}) async {
+    String formatedURL = '';
+    FormData formdata = FormData.fromMap({
+      "file": await MultipartFile.fromFile(path, filename: name),
+    });
+    String uploadPath = configMap['uploadPath'];
+    String token = configMap['token'];
+    String today = getToday('yyyyMMdd');
+    String alistToday = await Global.getTodayAlistUpdate();
+    if (alistToday != today) {
+      var res = await AlistManageAPI.getToken(
+          configMap['host'], configMap['alistusername'], configMap['password']);
+      if (res[0] == 'success') {
+        token = res[1];
+        String sqlResult = '';
+        try {
+          List sqlconfig = [];
+          sqlconfig.add(configMap['host']);
+          sqlconfig.add(configMap['alistusername']);
+          sqlconfig.add(configMap['password']);
+          sqlconfig.add(token);
+          sqlconfig.add(uploadPath);
+          String defaultUser = await Global.getUser();
+          sqlconfig.add(defaultUser);
+          var queryalist = await MySqlUtils.queryAlist(username: defaultUser);
+          var queryuser = await MySqlUtils.queryUser(username: defaultUser);
+          if (queryuser == 'Empty') {
+            return ['failed'];
+          } else if (queryalist == 'Empty') {
+            sqlResult = await MySqlUtils.insertAlist(content: sqlconfig);
+          } else {
+            sqlResult = await MySqlUtils.updateAlist(content: sqlconfig);
+          }
+        } catch (e) {
+          return ['failed'];
+        }
+        if (sqlResult == "Success") {
+          final alistConfig = AlistConfigModel(
+            configMap['host'],
+            configMap['alistusername'],
+            configMap['password'],
+            token,
+            uploadPath,
+          );
+          final alistConfigJson = jsonEncode(alistConfig);
+          final alistConfigFile = await AlistConfigState().localFile;
+          alistConfigFile.writeAsString(alistConfigJson);
+          await Global.setTodayAlistUpdate(today);
+        } else {
+          return ['failed'];
+        }
+      } else {
+        return ['failed'];
+      }
+    }
+    if (uploadPath == 'None') {
+      uploadPath = '/';
+    } else {
+      if (!uploadPath.startsWith('/')) {
+        uploadPath = '/$uploadPath';
+      }
+      if (!uploadPath.endsWith('/')) {
+        uploadPath = '$uploadPath/';
+      }
+    }
+    String filePath = uploadPath + name;
+
+    BaseOptions options = BaseOptions(
+      connectTimeout: 30000,
+      receiveTimeout: 30000,
+      sendTimeout: 30000,
+    );
+    File uploadFile = File(path);
+    int contentLength = await uploadFile.length().then((value) {
+      return value;
+    });
+    options.headers = {
+      "Authorization": token,
+      "Content-Type":
+          "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+      "file-path": Uri.encodeComponent(filePath),
+      "Content-Length": contentLength,
+    };
+    Dio dio = Dio(options);
+    String uploadUrl = configMap["host"] + "/api/fs/form";
+
+    try {
+      var response = await dio.put(uploadUrl, data: formdata);
+      if (response.statusCode == 200 &&
+          response.data!['message'] == 'success') {
+        String infoGetUrl = configMap["host"] + "/api/fs/get";
+        String refreshUrl = configMap["host"] + "/api/fs/list";
+        BaseOptions getOptions = BaseOptions(
+          connectTimeout: 30000,
+          receiveTimeout: 30000,
+          sendTimeout: 30000,
+        );
+        getOptions.headers = {
+          "Authorization": configMap["token"],
+          "Content-Type": "application/json",
+        };
+        Dio dioGet = Dio(getOptions);
+        Dio dioRefresh = Dio(getOptions);
+        Map getformData = {
+          "path": filePath,
+        };
+        Map refreshListFormData = {
+          "password": "",
+          "page": 1,
+          "per_page": 1,
+          "path": uploadPath,
+          "refresh": true
+        };
+        var refreshResponse =
+            await dioRefresh.post(refreshUrl, data: refreshListFormData);
+        if (refreshResponse.statusCode == 200 &&
+            refreshResponse.data!['message'] == 'success') {
+          var responseGet = await dioGet.post(infoGetUrl, data: getformData);
+          if (responseGet.statusCode == 200 &&
+              responseGet.data['message'] == 'success') {
+            String returnUrl = responseGet.data!['data']['raw_url'];
+            //返回缩略图地址用来在相册显示
+            String displayUrl = responseGet.data!['data']['thumb'] == "" ||
+                    responseGet.data!['data']['thumb'] == null
+                ? returnUrl
+                : responseGet.data!['data']['thumb'];
+            Map pictureKeyMap = Map.from(configMap);
+            pictureKeyMap['sign'] = responseGet.data!['data']['sign'];
+            pictureKeyMap['uploadPath'] = uploadPath;
+            pictureKeyMap['filenames'] = name;
+            String pictureKey = jsonEncode(pictureKeyMap);
+            String hostPicUrl = responseGet.data!['data']['sign'] == "" ||
+                    responseGet.data!['data']['sign'] == null
+                ? returnUrl
+                : configMap['host'] +
+                    '/d' +
+                    filePath +
+                    '?sign=' +
+                    responseGet.data!['data']['sign'];
+
+            if (Global.isCopyLink == true) {
+              formatedURL =
+                  linkGenerateDict[Global.defaultLKformat]!(hostPicUrl, name);
+            } else {
+              formatedURL = hostPicUrl;
+            }
+            return [
+              "success",
+              formatedURL,
+              returnUrl,
+              pictureKey,
+              displayUrl,
+              hostPicUrl
+            ];
+          } else {
+            return ['failed'];
+          }
+        }
+      } else {
+        return ["failed"];
+      }
+    } catch (e) {
+      if (e is DioError) {
+        FLog.error(
+            className: "AlistImageUploadUtils",
+            methodName: "uploadApi",
+            text: formatErrorMessage({
+              'path': path,
+              'name': name,
+            }, e.toString(), isDioError: true, dioErrorMessage: e),
+            dataLogType: DataLogType.ERRORS.toString());
+      } else {
+        FLog.error(
+            className: "AlistImageUploadUtils",
+            methodName: "uploadApi",
+            text: formatErrorMessage({
+              'path': path,
+              'name': name,
+            }, e.toString()),
+            dataLogType: DataLogType.ERRORS.toString());
+      }
+      return [e.toString()];
+    }
+  }
+
+  static deleteApi({required Map deleteMap, required Map configMap}) async {
+    Map configMapFromPictureKey = jsonDecode(deleteMap['pictureKey']);
+    Map<String, dynamic> formdata = {
+      "dir": configMapFromPictureKey['uploadPath'],
+      "names": [configMapFromPictureKey['filenames']]
+    };
+    String uploadPath = configMap['uploadPath'];
+    String token = configMap['token'];
+    String today = getToday('yyyyMMdd');
+    String alistToday = await Global.getTodayAlistUpdate();
+    if (alistToday != today) {
+      var res = await AlistManageAPI.getToken(
+          configMap['host'], configMap['alistusername'], configMap['password']);
+      if (res[0] == 'success') {
+        token = res[1];
+        String sqlResult = '';
+        try {
+          List sqlconfig = [];
+          sqlconfig.add(configMap['host']);
+          sqlconfig.add(configMap['alistusername']);
+          sqlconfig.add(configMap['password']);
+          sqlconfig.add(token);
+          sqlconfig.add(uploadPath);
+          String defaultUser = await Global.getUser();
+          sqlconfig.add(defaultUser);
+          var queryalist = await MySqlUtils.queryAlist(username: defaultUser);
+          var queryuser = await MySqlUtils.queryUser(username: defaultUser);
+          if (queryuser == 'Empty') {
+            return ['failed'];
+          } else if (queryalist == 'Empty') {
+            sqlResult = await MySqlUtils.insertAlist(content: sqlconfig);
+          } else {
+            sqlResult = await MySqlUtils.updateAlist(content: sqlconfig);
+          }
+        } catch (e) {
+          return ['failed'];
+        }
+        if (sqlResult == "Success") {
+          final alistConfig = AlistConfigModel(
+            configMap['host'],
+            configMap['alistusername'],
+            configMap['password'],
+            token,
+            uploadPath,
+          );
+          final alistConfigJson = jsonEncode(alistConfig);
+          final alistConfigFile = await AlistConfigState().localFile;
+          alistConfigFile.writeAsString(alistConfigJson);
+          await Global.setTodayAlistUpdate(today);
+        } else {
+          return ['failed'];
+        }
+      } else {
+        return ['failed'];
+      }
+    }
+    BaseOptions options = BaseOptions(
+      connectTimeout: 30000,
+      receiveTimeout: 30000,
+      sendTimeout: 30000,
+    );
+    options.headers = {
+      "Authorization": configMap["token"],
+      "Content-Type": "application/json",
+    };
+    Dio dio = Dio(options);
+    String deleteUrl = configMapFromPictureKey["host"] + "/api/fs/remove";
+    try {
+      var response = await dio.post(deleteUrl, data: formdata);
+      if (response.statusCode == 200 &&
+          response.data!['message'] == "success") {
+        return [
+          "success",
+        ];
+      } else {
+        return ["failed"];
+      }
+    } catch (e) {
+      if (e is DioError) {
+        FLog.error(
+            className: "AlistImageUploadUtils",
+            methodName: "deleteApi",
+            text: formatErrorMessage({}, e.toString(),
+                isDioError: true, dioErrorMessage: e),
+            dataLogType: DataLogType.ERRORS.toString());
+      } else {
+        FLog.error(
+            className: "AlistImageUploadUtils",
+            methodName: "deleteApi",
+            text: formatErrorMessage({}, e.toString()),
+            dataLogType: DataLogType.ERRORS.toString());
+      }
+      return [e.toString()];
+    }
+  }
+}
