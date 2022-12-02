58d7ac32a0686ae37d68e677d3d7d963a91b1a36
萌萌哒赫萝 <ma_shiqing@163.com>
2022-12-02 15:23:34 +0800

    新增：Alist支持

--- lib/pages/upload_pages/upload_utils.dart
+++ lib/pages/upload_pages/upload_utils.dart
@@ -23,10 +23,13 @@ import 'package:horopic/pages/upload_pages/upload_task.dart';
 import 'package:horopic/utils/event_bus_utils.dart';
 import 'package:horopic/utils/uploader.dart';
 import 'package:horopic/utils/global.dart';
+import 'package:horopic/utils/sql_utils.dart';
 import 'package:horopic/utils/common_functions.dart';
 import 'package:horopic/album/album_sql.dart';
 import 'package:horopic/api/tencent_api.dart';
 import 'package:horopic/api/qiniu_api.dart';
+import 'package:horopic/picture_host_manage/manage_api/alist_manage_api.dart';
+import 'package:horopic/picture_host_configure/configure_page/alist_configure.dart';
 
 class UploadManager {
   final Map<String, UploadTask> _cache = <String, UploadTask>{};
@@ -1138,8 +1141,13 @@ class UploadManager {
             displayUrl = '$customUrl$urlpath';
           }
         } else {
-          returnUrl = 'https://$endpoint/$urlpath';
-          displayUrl = 'https://$endpoint/$urlpath';
+          if (endpoint.contains('amazonaws.com')) {
+            returnUrl = 'https://$bucket.s3.$region.amazonaws.com/$urlpath';
+            displayUrl = 'https://$bucket.s3.$region.amazonaws.com/$urlpath';
+          } else {
+            returnUrl = 'https://$bucket.$endpoint/$urlpath';
+            displayUrl = 'https://$bucket.$endpoint/$urlpath';
+          }
         }
 
         String formatedURL = '';
@@ -1168,6 +1176,181 @@ class UploadManager {
         await AlbumSQL.insertData(Global.imageDBExtend!,
             pBhostToTableName[Global.defaultPShost]!, maps);
         setStatus(task, UploadStatus.completed);
+      } else if (defaultPH == 'alist') {
+        String uploadPath = configMap['uploadPath'];
+        String token = configMap['token'];
+        String today = getToday('yyyyMMdd');
+        String alistToday = await Global.getTodayAlistUpdate();
+        if (alistToday != today) {
+          var res = await AlistManageAPI.getToken(configMap['host'],
+              configMap['alistusername'], configMap['password']);
+          if (res[0] == 'success') {
+            token = res[1];
+            String sqlResult = '';
+            try {
+              List sqlconfig = [];
+              sqlconfig.add(configMap['host']);
+              sqlconfig.add(configMap['alistusername']);
+              sqlconfig.add(configMap['password']);
+              sqlconfig.add(token);
+              sqlconfig.add(uploadPath);
+              String defaultUser = await Global.getUser();
+              sqlconfig.add(defaultUser);
+              var queryalist =
+                  await MySqlUtils.queryAlist(username: defaultUser);
+              var queryuser = await MySqlUtils.queryUser(username: defaultUser);
+              if (queryuser == 'Empty') {
+                throw 'Empty';
+              } else if (queryalist == 'Empty') {
+                sqlResult = await MySqlUtils.insertAlist(content: sqlconfig);
+              } else {
+                sqlResult = await MySqlUtils.updateAlist(content: sqlconfig);
+              }
+            } catch (e) {
+              rethrow;
+            }
+            if (sqlResult == "Success") {
+              final alistConfig = AlistConfigModel(
+                configMap['host'],
+                configMap['alistusername'],
+                configMap['password'],
+                token,
+                uploadPath,
+              );
+              final alistConfigJson = jsonEncode(alistConfig);
+              final alistConfigFile = await AlistConfigState().localFile;
+              alistConfigFile.writeAsString(alistConfigJson);
+              await Global.setTodayAlistUpdate(today);
+            } else {
+              throw 'sqlError';
+            }
+          } else {
+            throw 'tokenError';
+          }
+        }
+
+        if (uploadPath != 'None') {
+          if (uploadPath.startsWith('/')) {
+            uploadPath = uploadPath.substring(1);
+          }
+          if (!uploadPath.endsWith('/')) {
+            uploadPath = '$uploadPath/';
+          }
+        } else {
+          uploadPath = '/';
+        }
+        //云存储的路径
+        String filePath = uploadPath + fileName;
+
+        BaseOptions options = BaseOptions(
+          connectTimeout: 30000,
+          receiveTimeout: 30000,
+          sendTimeout: 30000,
+        );
+        File uploadFile = File(path);
+        int contentLength = await uploadFile.length().then((value) {
+          return value;
+        });
+        options.headers = {
+          "Authorization": token,
+          "Content-Type":
+              "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+          "file-path": Uri.encodeComponent(filePath),
+          "Content-Length": contentLength,
+        };
+        Dio dio = Dio(options);
+        String uploadUrl = configMap["host"] + "/api/fs/form";
+        FormData formdata = FormData.fromMap({
+          "file": await MultipartFile.fromFile(path, filename: fileName),
+        });
+        var response = await dio.put(
+          uploadUrl,
+          data: formdata,
+          onSendProgress: createCallback(path, fileName),
+        );
+        if (response.statusCode == HttpStatus.ok &&
+            response.data!['message'] == 'success') {
+          String infoGetUrl = configMap["host"] + "/api/fs/get";
+          String refreshUrl = configMap["host"] + "/api/fs/list";
+          BaseOptions getOptions = BaseOptions(
+            connectTimeout: 30000,
+            receiveTimeout: 30000,
+            sendTimeout: 30000,
+          );
+          getOptions.headers = {
+            "Authorization": configMap["token"],
+            "Content-Type": "application/json",
+          };
+          Dio dioGet = Dio(getOptions);
+          Dio dioRefresh = Dio(getOptions);
+          Map getformData = {
+            "path": filePath,
+          };
+          Map refreshListFormData = {
+            "password": "",
+            "page": 1,
+            "per_page": 1,
+            "path": uploadPath,
+            "refresh": true
+          };
+          var refreshResponse =
+              await dioRefresh.post(refreshUrl, data: refreshListFormData);
+          if (refreshResponse.statusCode == 200 &&
+              refreshResponse.data!['message'] == 'success') {
+            var responseGet = await dioGet.post(infoGetUrl, data: getformData);
+            if (responseGet.statusCode == 200 &&
+                responseGet.data['message'] == 'success') {
+              eventBus.fire(AlbumRefreshEvent(albumKeepAlive: false));
+              Map<String, dynamic> maps = {};
+
+              String returnUrl = responseGet.data!['data']['raw_url'];
+              //返回缩略图地址用来在相册显示
+              String displayUrl = responseGet.data!['data']['thumb'] == "" ||
+                      responseGet.data!['data']['thumb'] == null
+                  ? returnUrl
+                  : responseGet.data!['data']['thumb'];
+              String hostPicUrl = responseGet.data!['data']['sign'] == "" ||
+                      responseGet.data!['data']['sign'] == null
+                  ? returnUrl
+                  : configMap['host'] +
+                      '/d' +
+                      filePath +
+                      '?sign=' +
+                      responseGet.data!['data']['sign'];
+
+              String formatedURL = '';
+              if (Global.isCopyLink == true) {
+                formatedURL = linkGenerateDict[Global.defaultLKformat]!(
+                    hostPicUrl, fileName);
+              } else {
+                formatedURL = hostPicUrl;
+              }
+              await Clipboard.setData(ClipboardData(text: formatedURL));
+
+              Map pictureKeyMap = Map.from(configMap);
+              pictureKeyMap['sign'] = responseGet.data!['data']['sign'];
+              pictureKeyMap['uploadPath'] = uploadPath;
+              pictureKeyMap['filenames'] = fileName;
+              String pictureKey = jsonEncode(pictureKeyMap);
+              maps = {
+                'path': path,
+                'name': fileName,
+                'url': returnUrl,
+                'PBhost': Global.defaultPShost,
+                'pictureKey': pictureKey,
+                'hostSpecificArgA': displayUrl,
+                'hostSpecificArgB': hostPicUrl,
+              };
+              List letter = 'CDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+              for (int i = 0; i < letter.length; i++) {
+                maps['hostSpecificArg${letter[i]}'] = 'test';
+              }
+              await AlbumSQL.insertData(Global.imageDBExtend!,
+                  pBhostToTableName[Global.defaultPShost]!, maps);
+              setStatus(task, UploadStatus.completed);
+            }
+          }
+        }
       }
     } catch (e) {
       FLog.error(
